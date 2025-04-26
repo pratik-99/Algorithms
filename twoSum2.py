cc671957b8f033aefa1b7e1ec8c3299ccfa91e9a
Pratik poojary <pratik.poojary@somaiya.edu>
2025-04-26 22:33:12 +0530

    add two pointer solution to twoSum

--- twoSum2.py
+++ twoSum2.py
@@ -12,7 +12,7 @@ Example:
 
     Output: [1,2]
 """
-
+from typing import List
 # Brute Force 
 
 def twoSum(self, numbers: List[int], target: int) -> List[int]:
@@ -22,4 +22,16 @@ def twoSum(self, numbers: List[int], target: int) -> List[int]:
                     break
                 if(numbers[i]+numbers[j]==target):
                     return [i+1,j+1]
-                    
+#Two Pointer method
+
+def twoSumTwoPointer(self, numbers:List[int], target: int):
+    l=0
+    r=len(numbers)-1
+    while l<r:
+        if(numbers[l] + numbers[r] < target):
+            l+=1 
+            
+        elif(numbers[l]+numbers[r]>target):
+            r-=1
+        else:
+            return [l+1,r+1]                   
