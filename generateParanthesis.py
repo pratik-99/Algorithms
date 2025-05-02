9f1214388131d0a97cd2860ca57a54b3a67957ab
Pratik poojary <pratik.poojary@somaiya.edu>
2025-05-03 00:03:35 +0530

    add solution to generate parenthesis

---
+++ generateParanthesis.py
@@ -0,0 +1,46 @@
+"""
+You are given an integer n. Return all well-formed parentheses strings that you can generate with n pairs of parentheses.
+
+Example 1:
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+
+Example 2:
+Input: n = 1
+Output: ["()"]
+"""
+
+class Solution:
+    def generateParenthesis(self, n: int) -> list[str]:
+        res = []
+        def backtrack(S = '', left = 0, right = 0):
+            if len(S) == 2*n:
+                res.append(S)
+                return
+            if left < n:
+                backtrack(S + '(', left + 1, right)
+            if right < left:
+                backtrack(S + ')', left, right + 1)
+        backtrack()
+        return res
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        stack=[]
+        res=[]
+
+        def dfs (openB, closeB):
+            if openB == closeB == n:
+                res.append("".join(stack))
+                return
+            if openB < n:
+                stack.append('(')
+                dfs(openB+1, closeB)
+                stack.pop()
+            if closeB < openB:
+                stack.append(')')
+                dfs(openB, closeB+1)
+                stack.pop()
+
+        dfs(0,0)
+        return res         
